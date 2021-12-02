5ce8d2f3af710e51689d56e108d70e715e8fc66c
Adam Geiger <ageiger@us.ibm.com>
2021-12-02 15:08:47 -0500

    update module

--- modules/instance/outputs.tf
+++ modules/instance/outputs.tf
@@ -1,3 +1,4 @@
 output "instance_info" {
-   value = data.ibm_resource_instance.kms_instance
+   value =  (var.kms_provision && var.kms_service == "keyprotect" ?  ibm_resource_instance.kms_instance[0].guid : 
+      (var.kms_provision && var.kms_service == "hpcs" ? ibm_hpcs.hpcs[0].guid : data.ibm_resource_instance.kms_instance[0].guid)) 
 } 
