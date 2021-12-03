46c8b08b95de1b46ce9705c07efab248a35cc632
Adam Geiger <ageiger@us.ibm.com>
2021-12-03 10:32:31 -0500

    Change output to guid

--- modules/instance/outputs.tf
+++ modules/instance/outputs.tf
@@ -1,4 +1,4 @@
-output "instance_info" {
+output "guid" {
    value =  (var.kms_provision && var.kms_service == "keyprotect" ?  ibm_resource_instance.kms_instance[0].guid : 
       (var.kms_provision && var.kms_service == "hpcs" ? ibm_hpcs.hpcs[0].guid : data.ibm_resource_instance.kms_instance[0].guid)) 
 } 
