068bef75c0f8cdc66036f4079375464e6ab8ea32
Adam Geiger <ageiger@us.ibm.com>
2021-12-06 11:32:18 -0500

    Revert "update module"
    This reverts commit 5ce8d2f3af710e51689d56e108d70e715e8fc66c.

--- modules/instance/outputs.tf
+++ modules/instance/outputs.tf
@@ -1,4 +1,3 @@
-output "guid" {
-   value =  (var.kms_provision && var.kms_service == "keyprotect" ?  ibm_resource_instance.kms_instance[0].guid : 
-      (var.kms_provision && var.kms_service == "hpcs" ? ibm_hpcs.hpcs[0].guid : data.ibm_resource_instance.kms_instance[0].guid)) 
+output "instance_info" {
+   value = data.ibm_resource_instance.kms_instance
 } 
